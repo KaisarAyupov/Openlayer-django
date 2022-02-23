035de87a21ee49e58726f34183eccb1032c9ac6d
caesar699 <caesar699@gmail.com>
2022-02-23 09:01:37 +0600

    DATABASES

--- gisApp/gisApp/settings.py
+++ gisApp/gisApp/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'gisApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'gisapp',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
