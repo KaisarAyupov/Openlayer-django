a3421c49e631c02f28f21b71132e3c3a92dfe5ad
caesar699 <caesar699@gmail.com>
2022-02-28 13:48:48 +0600

    штвуч

--- gisApp/gisApp/static/js/main.js
+++ gisApp/gisApp/static/js/main.js
@@ -1,79 +1,71 @@
-(function () {
+var mapView = new ol.View({
+    center: ol.proj.fromLonLat([68.804138, 48.946857]),
+    zoom: 5.5
+});
 
-    var mapView = new ol.View({
-        center: ol.proj.fromLonLat([68.804138, 48.946857]),
-        zoom: 5.5
-    });
+var map = new ol.Map({
+    target: 'map',
+    view: mapView
+});
 
-    var map = new ol.Map({
-        target: 'map',
-        view: mapView
-    });
+var noneTile = new ol.layer.Tile({
+    title: 'None',
+    type: 'base',
+    visible: false
+});
 
-    var noneTile = new ol.layer.Tile({
-        title: 'None',
-        type: 'base',
-        visible: false
-    });
+var osmTile = new ol.layer.Tile({
+    title: 'Open Street Map',
+    visible: true,
+    type: 'base',
+    source: new ol.source.OSM()
+});
 
-    var osmTile = new ol.layer.Tile({
-        title: 'Open Street Map',
-        visible: true,
-        type: 'base',
-        source: new ol.source.OSM()
-    });
+// map.addLayer(osmTile);
+var baseGroup = new ol.layer.Group({
+    title: 'Base Maps',
+    fold: true,
+    layers: [osmTile, noneTile]
+});
+map.addLayer(baseGroup);
 
-    // map.addLayer(osmTile);
-    var baseGroup = new ol.layer.Group({
-        title: 'Base Maps',
-        fold: true,
-        layers: [osmTile, noneTile]
-    });
-    map.addLayer(baseGroup);
+var DistrictKZTile = new ol.layer.Tile({
+    title: "DistrictKZ",
+    source: new ol.source.TileWMS({
+        url: 'https://geoportal.ingeo.kz/geoserver/geonode/wms',
+        params: { 'LAYERS': 'geonode:DistrictKZ_CH', 'TILED': true },
+        serverType: 'geoserver',
+        visible: true
+    })
+});
 
-    var DistrictKZTile = new ol.layer.Tile({
-        title: "DistrictKZ",
-        source: new ol.source.TileWMS({
-            url: 'https://geoportal.ingeo.kz/geoserver/geonode/wms',
-            params: { 'LAYERS': 'geonode:DistrictKZ_CH', 'TILED': true },
-            serverType: 'geoserver',
-            visible: true
-        })
-    });
+//map.addLayer(DistrictKZTile);
 
-    //map.addLayer(DistrictKZTile);
+var WmbPolTile = new ol.layer.Tile({
+    title: "VXB",
+    source: new ol.source.TileWMS({
+        url: 'https://geoportal.ingeo.kz/geoserver/geonode/wms',
+        params: { 'LAYERS': 'geonode:wmb_polygon', 'TILED': true },
+        serverType: 'geoserver',
+        visible: true
+    })
+});
 
-    var WmbPolTile = new ol.layer.Tile({
-        title: "VXB",
-        source: new ol.source.TileWMS({
-            url: 'https://geoportal.ingeo.kz/geoserver/geonode/wms',
-            params: { 'LAYERS': 'geonode:wmb_polygon', 'TILED': true },
-            serverType: 'geoserver',
-            visible: true
-        })
-    });
+//map.addLayer(WmbPolTile);
 
-    //map.addLayer(WmbPolTile);
+var overlayGroup = new ol.layer.Group({
+    title: 'Overlays',
+    fold: true,
+    layers: [DistrictKZTile, WmbPolTile]
+});
 
-    var overlayGroup = new ol.layer.Group({
-        title: 'Overlays',
-        fold: true,
-        layers: [DistrictKZTile, WmbPolTile]
-    });
+//map.addLayer(overlayGroup);
+var layerSwitcher = new ol.control.LayerSwitcher({
+    activationMode: 'click',
+    startActive: false,
+    groupSelectStyle: 'children'
+});
 
-    //map.addLayer(overlayGroup);
-    var layerSwitcher = new ol.control.LayerSwitcher({
-        activationMode: 'click',
-        startActive: false,
-        groupSelectStyle: 'children'
-    });
+map.addControl(layerSwitcher);
 
-    map.addControl(layerSwitcher);
 
-    var mousePosition = new ol.control.MousePosition({
-        className: 'mousePosition',
-        projection: 'EPSG:4326',
-        coordinateFormat: function (coordinate) { return ol.coordinate.format(coordinate, '{y} , {x}', 6); }
-    });
-
-})();
