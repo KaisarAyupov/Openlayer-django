7e178447cfa0dce6eba7337f1f7ace5b88d369bb
caesar699 <caesar699@gmail.com>
2022-03-30 14:14:18 +0600

    lib

--- gisApp/apps/feature/models.py
+++ gisApp/apps/feature/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+import datetime
+
+# The shapefile model
+class Shp(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000, blank=True)
+    file = models.FileField(upload_to='%Y/%m/%d')
+    uploaded_date = models.DateField(default=datetime.date.today, blank=True)
+
+    def __str__(self):
+        return self.name
