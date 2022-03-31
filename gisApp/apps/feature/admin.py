1b26e86f6aaf8d1da7bc1d9b3a0629fd83f9bdcc
caesar699 <caesar699@gmail.com>
2022-03-31 10:47:31 +0600

    setup postgis

--- gisApp/apps/feature/admin.py
+++ gisApp/apps/feature/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Shp
 
-# Register your models here.
+admin.site.register(Shp)
--- gisApp/apps/feature/models.py
+++ gisApp/apps/feature/models.py
@@ -1,13 +1,27 @@
 from django.db import models
-
 import datetime
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+import geopandas as gpd
+import os
+import glob
+import zipfile
+from sqlalchemy import *
+from geoalchemy2 import Geometry, WKTElement
+from geo.Geoserver import Geoserver
+from geo.Postgres import Db
+
 
 # The shapefile model
 class Shp(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
     description = models.CharField(max_length=1000, blank=True)
     file = models.FileField(upload_to='%Y/%m/%d')
     uploaded_date = models.DateField(default=datetime.date.today, blank=True)
+
+    def get_url(self):
+        return reverse('shp_file', args=[self.slug])
 
     def __str__(self):
         return self.name
