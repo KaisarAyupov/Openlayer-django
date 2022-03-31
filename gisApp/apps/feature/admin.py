eee703fa9b09b7f9dcd97f04f3f9f3c2fe2bdf37
caesar699 <caesar699@gmail.com>
2022-03-31 11:20:08 +0600

    ShpAdmin

--- gisApp/apps/feature/admin.py
+++ gisApp/apps/feature/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Shp
 
-admin.site.register(Shp)
+class ShpAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Shp, ShpAdmin)
+
--- gisApp/apps/feature/models.py
+++ gisApp/apps/feature/models.py
@@ -1,15 +1,5 @@
 from django.db import models
 import datetime
-from django.db.models.signals import post_save, post_delete
-from django.dispatch import receiver
-import geopandas as gpd
-import os
-import glob
-import zipfile
-from sqlalchemy import *
-from geoalchemy2 import Geometry, WKTElement
-from geo.Geoserver import Geoserver
-from geo.Postgres import Db
 
 
 # The shapefile model
@@ -19,9 +9,6 @@ class Shp(models.Model):
     description = models.CharField(max_length=1000, blank=True)
     file = models.FileField(upload_to='%Y/%m/%d')
     uploaded_date = models.DateField(default=datetime.date.today, blank=True)
-
-    def get_url(self):
-        return reverse('shp_file', args=[self.slug])
 
     def __str__(self):
         return self.name
